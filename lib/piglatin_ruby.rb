4eaad78294081ecfa5d725a70845e823202e7717
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2014-03-03 17:09:26 -0800

    changes it to use a .each_char loop instead of a while loop

--- lib/piglatin_ruby.rb
+++ lib/piglatin_ruby.rb
@@ -1,26 +1,34 @@
 def piglatin(phrase)
   if phrase[0] =~ /[yY]/
     phrase = phrase[1..-1] + phrase[0]
-  end
-
-  while is_consonant(phrase[0]) do
-    if phrase[0] =~ /[qQ]/ && phrase[1] =~ /[uU]/
+  elsif phrase[0] =~ /[qQ]/ && phrase[1] =~ /[uU]/
       phrase = phrase[2..-1] + phrase[0..1]
-    else
-      phrase = phrase[1..-1] + phrase[0]
+  else
+    phrase.each_char do |x|
+      if is_vowel(x) 
+        break
+      else
+        phrase = phrase[1..-1] + phrase[0]
+      end
     end
   end
   phrase + "ay" 
 end
 
-def is_consonant(letter)
-  if letter =~ /[^aeiouy]/
+def is_vowel(letter)
+  if letter =~ /[aeiouy]/
     true
   end
 end
+
 
 puts piglatin("elephant")
 puts piglatin("candy")
 puts piglatin("psychology")
 puts piglatin("yours")
 puts piglatin("quest")
+
+# "cat".each_char do |i|
+#   puts i+","
+# end
+
